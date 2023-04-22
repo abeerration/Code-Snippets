627689cf1e5eb66f25f7f33730af784fffd553a0
abeerration <abeerration@gmail.com>
2023-04-22 20:38:39 +0500

    Create words-to-js-array.py

---
+++ Python/convert-words-in-text-file-to-javascript-array/words-to-js-array.py
@@ -0,0 +1,19 @@
+words = []
+
+# open JS file for writing
+fileJs = open('words.js', 'w')
+
+# read words file and transform all words into a JS array string
+fileTxt = open('words.txt', 'r')
+words = fileTxt.readlines()
+
+fileJs.write("words = [\n")
+count = 0
+for w in words:
+  count += 1
+  fileJs.write('  "' + w.strip() + '", \n');
+
+fileJs.write("];\n")
+
+fileTxt.close()
+fileJs.close()
