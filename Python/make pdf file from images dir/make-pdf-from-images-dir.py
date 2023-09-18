0e7acf27e521428dcff899fbebcae30f229aecfe
abeerration <abeerration@gmail.com>
2023-09-18 11:30:30 +0500

    Update make-pdf-from-images-dir.py

--- Python/make pdf file from images dir/make-pdf-from-images-dir.py
+++ Python/make pdf file from images dir/make-pdf-from-images-dir.py
@@ -8,32 +8,33 @@ import os, re
 import img2pdf
 
 def natural_sort(list, key=lambda s:s):
-    """
+  """
     Sort the list into natural alphanumeric order.
-    """
-    def get_alphanum_key_func(key):
-        convert = lambda text: int(text) if text.isdigit() else text 
-        return lambda s: [convert(c) for c in re.split('([0-9]+)', key(s))]
+  """
+  def get_alphanum_key_func(key):
+    convert = lambda text: int(text) if text.isdigit() else text 
+    return lambda s: [convert(c) for c in re.split('([0-9]+)', key(s))]
     sort_key = get_alphanum_key_func(key)
     list.sort(key=sort_key)
 
 dirname = "./images"
 imgs = []
 exts = [".jpg", ".jpeg", ".png", ".webp", ".gif"]
-# currImg = 0
+currImg = 0
 
 for fname in os.listdir(dirname):
-    for ext in exts:
-        if fname.endswith(ext):
-            path = os.path.join(dirname, fname)
-            if os.path.isdir(path):
-                continue
-            imgs.append(path)
-            # with open(fname + ".pdf", "wb") as f:
-            #   f.write(img2pdf.convert(imgs[currImg]))
-            # currImg += 1
+  for ext in exts:
+    if fname.endswith(ext):
+      path = os.path.join(dirname, fname)
+      if os.path.isdir(path):
+        continue
+      imgs.append(path)
+      ## uncomment for generating individual PDFs
+      # with open(fname + ".pdf", "wb") as f:
+      #   f.write(img2pdf.convert(imgs[currImg]))
+      # currImg += 1
 
 natural_sort(imgs)
 
 with open("images-merged.pdf", "wb") as f:
-    f.write(img2pdf.convert(imgs))
+  f.write(img2pdf.convert(imgs))
