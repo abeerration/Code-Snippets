94c49fafeec6c4f4536d717cb661053b025922bd
abeerration <abeerration@gmail.com>
2023-09-18 11:22:37 +0500

    Revert "Update make-pdf-from-images-dir.py"
    This reverts commit 031090747afc156dfa3315eaf1f74c27cbc24a6f.

--- Python/make pdf file from images dir/make-pdf-from-images-dir.py
+++ Python/make pdf file from images dir/make-pdf-from-images-dir.py
@@ -8,32 +8,32 @@ import os, re
 import img2pdf
 
 def natural_sort(list, key=lambda s:s):
-  """
-  Sort the list into natural alphanumeric order.
-  """
-  def get_alphanum_key_func(key):
-    convert = lambda text: int(text) if text.isdigit() else text
-      return lambda s: [convert(c) for c in re.split('([0-9]+)', key(s))]
-        sort_key = get_alphanum_key_func(key)
-        list.sort(key=sort_key)
+    """
+    Sort the list into natural alphanumeric order.
+    """
+    def get_alphanum_key_func(key):
+        convert = lambda text: int(text) if text.isdigit() else text 
+        return lambda s: [convert(c) for c in re.split('([0-9]+)', key(s))]
+    sort_key = get_alphanum_key_func(key)
+    list.sort(key=sort_key)
 
-        dirname = "./images"
-        imgs = []
-        exts = [".jpg", ".jpeg", ".png", ".webp", ".gif"]
-        # currImg = 0
+dirname = "./images"
+imgs = []
+exts = [".jpg", ".jpeg", ".png", ".webp", ".gif"]
+# currImg = 0
 
-        for fname in os.listdir(dirname):
-          for ext in exts:
-            if fname.endswith(ext):
-              path = os.path.join(dirname, fname)
-              if os.path.isdir(path):
+for fname in os.listdir(dirname):
+    for ext in exts:
+        if fname.endswith(ext):
+            path = os.path.join(dirname, fname)
+            if os.path.isdir(path):
                 continue
-                imgs.append(path)
-                # with open(fname + ".pdf", "wb") as f:
-                #   f.write(img2pdf.convert(imgs[currImg]))
-                # currImg += 1
+            imgs.append(path)
+            # with open(fname + ".pdf", "wb") as f:
+            #   f.write(img2pdf.convert(imgs[currImg]))
+            # currImg += 1
 
-                natural_sort(imgs)
+natural_sort(imgs)
 
-                with open("images-merged.pdf", "wb") as f:
-                  f.write(img2pdf.convert(imgs))
+with open("images-merged.pdf", "wb") as f:
+    f.write(img2pdf.convert(imgs))
